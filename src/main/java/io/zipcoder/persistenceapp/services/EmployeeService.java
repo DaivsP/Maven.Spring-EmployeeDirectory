5dcbbb3872747cf1b546551eb68921223a8f4c6c
Davis <davisperchik@gmail.com>
2019-04-05 17:56:18 -0400

    completed employee service and controller

--- src/main/java/io/zipcoder/persistenceapp/services/EmployeeService.java
+++ src/main/java/io/zipcoder/persistenceapp/services/EmployeeService.java
@@ -1,5 +1,6 @@
 package io.zipcoder.persistenceapp.services;
 
+import io.zipcoder.persistenceapp.models.Employee;
 import io.zipcoder.persistenceapp.repositories.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,30 @@ public class EmployeeService {
 
     public EmployeeService (EmployeeRepo repo){
         this.repo = repo;
+    }
+
+    public Employee createEmployee(Employee employee){
+        return repo.save(employee);
+    }
+
+    public Employee readEmployee(Long id){
+        return repo.getOne(id);
+    }
+
+    public Employee updateEmployee (Long id, Employee employee){
+        Employee originalEmployee = repo.getOne(id);
+        originalEmployee.setFirstName(employee.getFirstName());
+        originalEmployee.setLastName(employee.getLastName());
+        originalEmployee.setTitle(employee.getTitle());
+        originalEmployee.setPhoneNumber(employee.getPhoneNumber());
+        originalEmployee.setEmail(employee.getEmail());
+        originalEmployee.setHireDate(employee.getHireDate());
+        originalEmployee.setManager(employee.getManager());
+        return repo.save(originalEmployee);
+    }
+
+    public Boolean deleteEmployee (Long id){
+        repo.delete(id);
+        return true;
     }
 }
